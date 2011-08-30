263e51d5d3d89dfc70f42a45ade075f53d88c04b
Jack Chu <kamuigt@gmail.com>
2011-08-30 14:39:38 -0400

    bump v0.0.9

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :campfire_notifications do
   description 'A plugin to display issue modifications to a Campfire room'
   url 'https://github.com/kamui/redmine-campfire_notifications'
   author_url 'http://jackchu.com'
-  version '0.0.8'
+  version '0.0.9'
 
   requires_redmine :version_or_higher => '0.8.0'
 end
